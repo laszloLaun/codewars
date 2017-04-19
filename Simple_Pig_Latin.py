298681a497417c547341d586d9ba2809859e6392
Laszlo Laun <laun.laszlo@gmail.com>
2017-04-19 17:31:51 +0200

    finished

--- Simple_Pig_Latin.py
+++ Simple_Pig_Latin.py
@@ -3,7 +3,6 @@ import string
 
 def pig_it(text):
     text = text.split(sep=" ")
-    print(text)
     pigged_text = []
     for word in text:
         if word in string.punctuation:
@@ -11,8 +10,4 @@ def pig_it(text):
         else:
             pigged_text.append(word[1:] + word[0:1] + "ay")
     pigged_text = " ".join(pigged_text)
-    print(pigged_text)
     return pigged_text
-
-
-pig_it('Quis custodiet ipsos custodes ?')
