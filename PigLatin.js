31c5fba35639b6934682c5821a00cf905b09a91a
Laszlo Laun <laun.laszlo@gmail.com>
2017-04-20 11:00:26 +0200

    solved

---
+++ PigLatin.js
@@ -0,0 +1,19 @@
+function pig_it(text) {
+    text = text.split(" ");
+    console.log(text);
+    punctuation = "!?";
+    pigged_text = [];
+    for (var i = 0; i < text.length; i++) {
+        if (text[i].includes("?")) {
+            pigged_text.push(text[i]);
+        }
+        else {
+            pigged_text.push(text[i].slice(1, text[i].length) + text[i].slice(0, 1) + "ay");
+        }
+    }
+    pigged_text = pigged_text.join(" ");
+    console.log(pigged_text);
+    return pigged_text;
+}
+
+pig_it('Quis custodiet ipsos custodes ?');
